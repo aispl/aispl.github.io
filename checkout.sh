3f291a48f530609bd19172f6a41eca2bd2d245a9
Tomasz Pik <tompik@gmail.com>
2013-10-10 09:45:08 +0200

    initial commit

---
+++ checkout.sh
@@ -0,0 +1,7 @@
+git clone git@github.com:aispl/ais-commons-bean.git
+git clone git@github.com:aispl/ais-commons-domain-us.git
+git clone git@github.com:aispl/ais-commons-infrastructure.git
+git clone git@github.com:aispl/ais-commons-domain.git
+git clone git@github.com:aispl/ais-commons-application.git
+git clone git@github.com:aispl/ais-commons-query.git
+
